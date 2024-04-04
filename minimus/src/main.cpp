3d4c458eb14e524081e231f11e9cee7727c82577
iniw <andrade.vinicius934@gmail.com>
2024-04-04 19:44:41 -0300

    test

--- minimus/src/main.cpp
+++ minimus/src/main.cpp
@@ -1,10 +1,28 @@
+#include "esp32-hal-gpio.h"
+#include "hal/gpio_types.h"
 #include <Arduino.h>
 #include <array>
 
-constexpr std::array<int, 3> order{ 1, 0, 3 };
+constexpr std::array<int, 3> password_order{ 1, 0, 2 };
+constexpr std::array<gpio_num_t, 3> buttons{ GPIO_NUM_18, GPIO_NUM_19, GPIO_NUM_21 };
 
 void setup() {
+    Serial.begin(115200);
+    for (auto i : buttons)
+        pinMode(i, INPUT_PULLUP);
+
+    pinMode(GPIO_NUM_14, OUTPUT);
 }
 
 void loop() {
+    analogWrite(GPIO_NUM_14, 128);
+
+    static auto tick = 0;
+
+    if (millis() - tick >= 1000) {
+        for (auto b : buttons)
+            Serial.println(digitalRead(b));
+
+        tick = millis();
+    }
 }
